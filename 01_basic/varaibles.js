eea0c7e3be7167ed054b555e3f020cd1e8cfc349
junedisani <124427068+junedisani@users.noreply.github.com>
2025-03-21 11:39:53 +0000

    variables

---
+++ 01_basic/varaibles.js
@@ -0,0 +1,8 @@
+const accountId = 12232
+let accountEmail = "juned@google.com"
+var accountPassword = "123456"
+accountCity = "mumbai"
+let accountState
+console.log (accountId)
+
+console.table ([accountId,accountEmail, accountPassword, accountCity, accountState])
