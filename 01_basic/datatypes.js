f80037384f4551b3571955ab237c324b8e9dc0c3
junedisani <124427068+junedisani@users.noreply.github.com>
2025-03-22 11:08:40 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all the js code as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser 
+
+console.log (3 + 3)
+console.log ("juned")
+
+
+let name = "juned"
+let age = 19
+let loggedin = false
+
+// number => 2 to power 53
+// bigint 
+// string = ""
+// boolean = true/false
+// null => standalone value
+// underfined =>
+// symbol => unique 
+
+
+// object
+// typeof => says which type of datatype
+
+console.log (typeof "juned")
+console.log (typeof 18)
+console.log (typeof undefined); // undefined
+console.log (typeof null); // object
