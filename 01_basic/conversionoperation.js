d17baf4072017493374ae77e3fd52f4607fcdc86
junedisani <124427068+junedisani@users.noreply.github.com>
2025-03-26 10:39:09 +0000

    conversion of data types

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,22 @@
+let score = 33
+
+console.log (typeof score)
+console.log (typeof (score))
+
+let valueInNumber = Number(score)
+console.log (typeof valueInNumber)
+console.log (valueInNumber)
+
+// 33=> 33
+// "33abc" => NaN
+// true => 1 ; // false => 0;
+
+
+let value = "juneed"
+let booleanValue = Boolean(value)
+console.log (booleanValue)
+console.log (typeof booleanValue)
+
+// 1 => true // 0=> false
+// "" => false
+//"juned" => true
