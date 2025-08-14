a3d3de80e7c9e0fe4ac9a1dfea9408971e88a457
junedisani <junedisani1234@gmail.com>
2025-08-15 00:06:08 +0530

    learning API request and promise

---
+++ 10_classes_and_oops/02_oop.js
@@ -0,0 +1,14 @@
+const user = {
+    username : "juned",
+    SignIn : true,
+
+    getUserDetails: function(){
+        console.log("get user details from the database");
+        
+    } 
+}
+
+
+console.log(user);
+console.log(user.username);
+user.getUserDetails()
